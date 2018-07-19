6fd864e2901e7091341e13e441e6b14144d74b9f
Joshua Ling <joshling1919@gmail.com>
2018-07-19 11:53:29 -0700

    Make resolve function chainable

--- promise.js
+++ promise.js
@@ -13,24 +13,29 @@ class MyPromise {
 
     // the executor function takes 2 parameters, resolve() and reject()
     const resolve = (res) => {
-      console.log('IM CURRENTLY RESOLVING');
-      // A promise is considered "settled" when it is no longer
-      // pending, that is, when either `resolve()` or `reject()`
-      // was called once. Calling `resolve()` or `reject()` twice
-      // or calling `reject()` after `resolve()` was already called
-      // are no ops.
       if (this.$state !== 'PENDING') {
         return;
       }
+
+      // If `res` is a "thenable", lock in this promise to match the
+      // resolved or rejected state of the thenable.
+      const then = res != null ? res.then : null;
+      if (typeof then === 'function') {
+        return then(resolve, reject);
+      }
+
       this.$state = 'FULFILLED';
       this.$internalValue = res;
 
-      // If somebody called `.then()` while this promise was pending, need
-      // to call their `onFulfilled()` function
+      // If somebody called `.then()` while this functino was pending, need to
+      // call their `onFulfilled` function
       for (const { onFulfilled } of this.$chained) {
         onFulfilled(res);
       }
+
+      return res;
     };
+
     const reject = (err) => {
       if (this.$state !== 'PENDING') {
       }
@@ -85,16 +90,6 @@ class MyPromise {
         this.$chained.push({ onFulfilled: _onFulfilled, onRejected: _onRejected });
       }
     });
-    // if (this.$state === 'FULFILLED') {
-    //   console.log('ON FULFILLED');
-    //   onFulfilled(this.$internalValue);
-    // } else if (this.$state === 'REJECTED') {
-    //   console.log('ON REJECTED');
-    //   onRejected(this.$internalValue);
-    // } else {
-    //   console.log('ON CHAINED');
-    //   this.$chained.push({ onFulfilled, onRejected });
-    // }
   }
 }
 
@@ -107,11 +102,62 @@ class MyPromise {
 // way I should just test it and walk through the code until I understand it, and
 // then I can move on to coding up the next portion.
 
-const firstPromise = new MyPromise((resolve, reject) => {
-  console.log('RUNNING THE EXECUTOR FUNCTION');
-  // imagine if this were an api call, that took a second
-  // to respond with 'wubadubdub'
-  setTimeout(() => resolve('wubadubdub'), 1000);
-});
+// const firstPromise = new MyPromise((resolve, reject) => {
+//   console.log('RUNNING THE EXECUTOR FUNCTION');
+//   // imagine if this were an api call, that took a second
+//   // to respond with 'wubadubdub'
+//   setTimeout(() => resolve('wubadubdub'), 1000);
+// });
 
-firstPromise.then(successMessage => console.log(`wahoo ${successMessage}`));
+// firstPromise.then(successMessage => console.log(`wahoo ${successMessage}`));
+
+new MyPromise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000); // (*)
+})
+  .then((result) => {
+    console.log('HERE IS THE FIRST RESULT', result); // 1
+    return result * 2;
+  })
+  .then((result) => {
+    console.log('HERE IS THE SECOND RESULT', result); // 2
+    return result * 2;
+  })
+  .then((result) => {
+    console.log('HERE IS THE THIRD RESULT', result); // 4
+    return result * 2;
+  });
+
+// Old #then method that does not support promise chaining:
+// then(onFulfilled, onRejected) {
+//   if (this.$state === 'FULFILLED') {
+//     console.log('ON FULFILLED');
+//     onFulfilled(this.$internalValue);
+//   } else if (this.$state === 'REJECTED') {
+//     console.log('ON REJECTED');
+//     onRejected(this.$internalValue);
+//   } else {
+//     console.log('ON CHAINED');
+//     this.$chained.push({ onFulfilled, onRejected });
+//   }
+// }
+
+// old resolve method that does not support promise chaining:
+// const resolve = (res) => {
+//   console.log('IM CURRENTLY RESOLVING');
+//   // A promise is considered "settled" when it is no longer
+//   // pending, that is, when either `resolve()` or `reject()`
+//   // was called once. Calling `resolve()` or `reject()` twice
+//   // or calling `reject()` after `resolve()` was already called
+//   // are no ops.
+//   if (this.$state !== 'PENDING') {
+//     return;
+//   }
+//   this.$state = 'FULFILLED';
+//   this.$internalValue = res;
+
+//   // If somebody called `.then()` while this promise was pending, need
+//   // to call their `onFulfilled()` function
+//   for (const { onFulfilled } of this.$chained) {
+//     onFulfilled(res);
+//   }
+// };
